a71defcd3b35104e91e4ab296a4782c97b3c15b6
bosichong <bosichong@qq.com>
2023-08-23 05:28:09 +0000

    Deploying to gh-pages from @ bosichong/githubactionstest@09d98528cea3565b04b189a4d55a3741362c57cf 🚀

---
+++ _next/static/uDtx1lifY5GkoHn78xCRx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/8bab7d291160fce6.css","static/chunks/pages/index-2c4f67cb57983c55.js"],"/_error":["static/chunks/pages/_error-1d28e89ede5693a1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
